e39e02dbf45c55974c28d8107979f5dfaa44372e
Cole deBoer <pg27cole@vfs.com>
2024-11-28 18:00:51 -0800

    ADDED: enemies, more blocktypes, and one level

--- Backend/server.js
+++ Backend/server.js
@@ -13,97 +13,103 @@ app.use(cors());
 
 let levels = {};
 
-// loads a level 
+// loads a level
 app.get("/level/:id", (req, res) => {
-    const levelId = req.params.id;
-    const filePath = path.join(__dirname, "levels", `${levelId}.json`)
-    
-    fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-            console.error("Error reading level data: ", err);
-            return res.status(404).send("Level not found");
-        }
-        res.send(data);
-    });
+  const levelId = req.params.id;
+  const filePath = path.join(__dirname, "levels", `${levelId}.json`);
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading level data: ", err);
+      return res.status(404).send("Level not found");
+    }
+    res.send(data);
+  });
 });
 
 // Saves a level
 app.post("/level/:id", (req, res) => {
-    const levelId = req.params.id;
-    const filePath = path.join(__dirname, "levels", `${levelId}.json`);
-    const levelData = req.body;
+  const levelId = req.params.id;
+  const filePath = path.join(__dirname, "levels", `${levelId}.json`);
+  const levelData = req.body;
 
-    if (!Array.isArray(levelData) || levelData.length === 0) {
-        return res.status(404).send("Level data must be an non-empty array");
-    };
+  if (!Array.isArray(levelData) || levelData.length === 0) {
+    return res.status(404).send("Level data must be an non-empty array");
+  }
 
-    // level data, FILTER (NULL = no filter), how many indentation spaces
-    fs.writeFile(filePath, JSON.stringify(levelData, null, 2), (err) => {
-        if(err){
-            console.error("Error saving level data: ", err);
-            return res.status(500).send("Server error");
-        };
-        res.send("Level saved successfully"); // automatically sends a status of 200
-    });
+  // level data, FILTER (NULL = no filter), how many indentation spaces
+  fs.writeFile(filePath, JSON.stringify(levelData, null, 2), (err) => {
+    if (err) {
+      console.error("Error saving level data: ", err);
+      return res.status(500).send("Server error");
+    }
+    res.send("Level saved successfully"); // automatically sends a status of 200
+  });
 });
 
 // renames a level
 app.put("/level/:id", (req, res) => {
-    const levelId = req.params.id;
-    const oldFilePath = path.join(__dirname, "levels", `${levelId}.json`);
-    const newFilePath = path.join(__dirname, "levels", `${req.body["name"]}.json`);
+  const levelId = req.params.id;
+  const oldFilePath = path.join(__dirname, "levels", `${levelId}.json`);
+  const newFilePath = path.join(
+    __dirname,
+    "levels",
+    `${req.body["name"]}.json`
+  );
 
-    if(fs.existsSync(newFilePath))
-    {
-        res.status(500).send("This name already exists. you cannot rename a file to this name wihout first deleting or renaming the already existing file.");
-        return;
+  if (fs.existsSync(newFilePath)) {
+    res
+      .status(500)
+      .send(
+        "This name already exists. you cannot rename a file to this name wihout first deleting or renaming the already existing file."
+      );
+    return;
+  }
+
+  fs.rename(oldFilePath, newFilePath, (err) => {
+    if (err) {
+      console.error("Error renaming level: ", err);
+      return res.status(500).send("Server error");
     }
-
-    fs.rename(oldFilePath, newFilePath, (err) => {
-        if(err) {
-            console.error("Error renaming level: ", err);
-            return res.status(500).send("Server error");
-        }
-        res.send(`Level Successfully renamed to ${req.body["name"]}`);
-    });
-})
+    res.send(`Level Successfully renamed to ${req.body["name"]}`);
+  });
+});
 
 // deletes a level
 app.delete("/level/:id", (req, res) => {
-    const levelId = req.params.id;
-    const filePath = path.join(__dirname, "levels", `${levelId}.json`);
+  const levelId = req.params.id;
+  const filePath = path.join(__dirname, "levels", `${levelId}.json`);
 
-    fs.unlink(filePath, (err) => {
-        if(err) {
-            console.error("Error removing level: ", err);
-            return res.status(500).send("Server error");
-        }
-    })
-    res.send("Level successfully deleted");
-})
-
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error removing level: ", err);
+      return res.status(500).send("Server error");
+    }
+  });
+  res.send("Level successfully deleted");
+});
 
 // get all levels at the same time
 app.get("/levels", (req, res) => {
-    fs.readdir("levels", (err, files) => {
-        if (err) {
-            console.error("Error reading levels directory:", err);
-            return res.status(500).send("Server error");
-        };
+  fs.readdir("levels", (err, files) => {
+    if (err) {
+      console.error("Error reading levels directory:", err);
+      return res.status(500).send("Server error");
+    }
 
-        const levelIds = files  
-            .filter(file => file.endsWith(".json")) // filters out all files that dont end with .json
-            .map(file => path.basename(file, ".json"));
+    const levelIds = files
+      .filter((file) => file.endsWith(".json")) // filters out all files that dont end with .json
+      .map((file) => path.basename(file, ".json"));
 
-            res.json(levelIds);
-    });
+    res.json(levelIds);
+  });
 });
 
 //checks to make sure levels folder exists, creates it if it doesnt
-if(!fs.existsSync("levels")) {
-   fs.mkdirSync("levels"); 
-};
+if (!fs.existsSync("levels")) {
+  fs.mkdirSync("levels");
+}
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- Frontend/editor.js
+++ Frontend/editor.js
@@ -1,235 +1,268 @@
 $(function () {
-    let blockCounter = 0;
-    function clearBlocks() {
-        let editorWindow = document.querySelector("#editor")
-        editorWindow.innerHTML = '';
-    }
+  let blockCounter = 0;
+  function clearBlocks() {
+    let editorWindow = document.querySelector("#editor");
+    editorWindow.innerHTML = "";
+  }
 
-    function loadLevelList() {
-        $.ajax({
-            url: "http://localhost:3000/levels",
-            method: "GET",
-            success: function (levelIds) {
-                const $levelList = $("#level-list");
-                $levelList.empty();
-                $levelList.append('<option value="">Select a Level</option>');
-                levelIds.forEach(function (id) {
-                    $levelList.append(`<option value="${id}">${id}</option>`)
-                });
-            },
-            error: function(xhr, status, error) {
-                console.error("Error fetching level list:", error);
-            }
-        });
-    };
-
-    function createBlock(type, css)
-    {
-        const blockId = `block-${blockCounter++}`;
-        
-        const block = $("<div></div>") // this would be for creating a div
-            .addClass(`${type}`)
-            .attr("id", blockId)
-            .css(css)
-            .appendTo("#editor");
-
-        block.draggable({
-            containment: "#editor",
-
-            stop: function (event, ui) {
-                // You can add stuff here :D
-                //TODO: some sort of soft saving
-            }
-        });
-
-        block.click(function (event) {
-            event.stopPropagation();
-        });
-    
-        block.contextmenu(function (event) {
-            event.preventDefault();
-            if(confirm("Delete this block?")) {
-                $(this).remove();
-            };
-        });
-    }
-
-    $("#add-block").click( () => createBlock("block", 
-        {
-            top: 0,
-            left: 0
-        }));
-
-    $("#clear-blocks").click ( () => {
-
-        if(confirm("Are you sure you want to clear all blocks from the window?"))
-        {
-            clearBlocks()
-        }
-    });
-
-    $("#load-level").click(() => {
+  function loadLevelList() {
+    $.ajax({
+      url: "http://localhost:3000/levels",
+      method: "GET",
+      success: function (levelIds) {
         const $levelList = $("#level-list");
-        const selectedElement = document.querySelector('#level-list');
-        const level = selectedElement.options[selectedElement.selectedIndex].value;
-
-        if(!level)
-        {
-            alert("Please enter a level");
-            return;
-        }
-
-        if($levelList.find(level) != undefined)
-        {
-            if(!confirm(`Would you like to load in ${level}?`)) return;
-            $.ajax({
-                url: 'http://localhost:3000/level/' + encodeURIComponent(level),
-                method: "GET",
-                contentType: "application/json",
-                success: function (response) {
-                    const JSONResponse = JSON.parse(response)
-                    clearBlocks();
-                    for(let i = 0; i < JSONResponse.length; i++)
-                    {
-                        const xPos = JSONResponse[i]["x"];
-                        const yPos = JSONResponse[i]["y"];
-                        const type = JSONResponse[i]["type"]
-                        
-                        createBlock(type, {
-                            left: xPos,
-                            top: yPos,
-                        });
-                    }
-                },
-                error: function (xhr, status, error) {
-                    alert("error loading level: " + xhr.responseText);
-                }
-            });
-        }
-    })
-
-    $("#save-level").click( () => {
-        const levelId = prompt("Enter a level name")
-        
-        const levelData = [];
-        
-        $(".block").each(function () {
-            const $this = $(this);
-            const position = $this.position();
-            levelData.push({
-                id: $this.attr("id"),
-                x: position.left,
-                y: position.top, 
-                width: $this.width(),
-                height: $this.height(),
-                type: "block"    
-            });
+        $levelList.empty();
+        $levelList.append('<option value="">Select a Level</option>');
+        levelIds.forEach(function (id) {
+          $levelList.append(`<option value="${id}">${id}</option>`);
         });
-        
-        if(levelData.length === 0) {
-            alert("The level is empty. Add some blocks before saving.");
-            return;
-        }
-        
-        if(!levelId) {
-            alert("Pleave enter a level ID");
-            return;         
-        }
-        
-        $.ajax({
-            url: 'http://localhost:3000/level/' + encodeURIComponent(levelId),
-            method: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(levelData),
-            success: function (response) {
-                alert(response);
-                loadLevelList();
-            },
-            error: function (xhr, status, error) {
-                alert("Error saving level: " + xhr.responseText);
-            }
-        });
+      },
+      error: function (xhr, status, error) {
+        console.error("Error fetching level list:", error);
+      },
+    });
+  }
+
+  function createBlock(type, css) {
+    const blockId = `block-${blockCounter++}`;
+
+    const block = $("<div></div>") // this would be for creating a div
+      .addClass(`${type}`)
+      .attr("id", blockId)
+      .css(css)
+      .appendTo("#editor");
+
+    block.draggable({
+      containment: "#editor",
+      stop: function (event, ui) {},
     });
 
-    //not getting server response, but it works
-    $("#rename-level").click( () => {
+    block.click(function (event) {
+      event.stopPropagation();
+    });
 
-        const options = $("#additive-options");
-        const renameInput = $(`
+    block.contextmenu(function (event) {
+      event.preventDefault();
+      if (confirm("Delete this block?")) {
+        $(this).remove();
+      }
+    });
+  }
+
+  $("#add-static-block").click(() =>
+    createBlock("static-block", {
+      top: 0,
+      left: 0,
+    })
+  );
+
+  $("#add-dynamic-block").click(() =>
+    createBlock("dynamic-block", {
+      top: 0,
+      left: 0,
+    })
+  );
+
+  $("#add-small-enemy").click(() =>
+    createBlock("small-enemy", {
+      top: 0,
+      left: 0,
+    })
+  );
+
+  $("#add-large-enemy").click(() =>
+    createBlock("large-enemy", {
+      top: 0,
+      left: 0,
+    })
+  );
+
+  $("#add-circle").click(() =>
+    createBlock("circle", {
+      top: 0,
+      left: 0,
+    })
+  );
+
+  $("#clear-blocks").click(() => {
+    if (confirm("Are you sure you want to clear all blocks from the window?")) {
+      clearBlocks();
+    }
+  });
+
+  $("#load-level").click(() => {
+    const $levelList = $("#level-list");
+    const selectedElement = document.querySelector("#level-list");
+    const level = selectedElement.options[selectedElement.selectedIndex].value;
+
+    if (!level) {
+      alert("Please enter a level");
+      return;
+    }
+
+    if ($levelList.find(level) != undefined) {
+      if (!confirm(`Would you like to load in ${level}?`)) return;
+      $.ajax({
+        url: "http://localhost:3000/level/" + encodeURIComponent(level),
+        method: "GET",
+        contentType: "application/json",
+        success: function (response) {
+          const JSONResponse = JSON.parse(response);
+          clearBlocks();
+          for (let i = 0; i < JSONResponse.length; i++) {
+            const xPos = JSONResponse[i]["x"];
+            const yPos = JSONResponse[i]["y"];
+            const type = JSONResponse[i]["type"];
+
+            createBlock(type, {
+              left: xPos,
+              top: yPos,
+            });
+          }
+        },
+        error: function (xhr, status, error) {
+          alert("error loading level: " + xhr.responseText);
+        },
+      });
+    }
+  });
+
+  function CreateLevel(levelData, querySelector, JSONType) {
+    $(querySelector).each(function () {
+      const $this = $(this);
+      const position = $this.position();
+      levelData.push({
+        id: $this.attr("id"),
+        x: position.left,
+        y: position.top,
+        width: $this.width(),
+        height: $this.height(),
+        type: JSONType,
+      });
+    });
+  }
+
+  $("#save-level").click(() => {
+    const levelId = prompt("Enter a level name");
+
+    const levelData = [];
+    CreateLevel(levelData, ".static-block", "static-block");
+    CreateLevel(levelData, ".dynamic-block", "dynamic-block");
+    CreateLevel(levelData, ".small-enemy", "small-enemy");
+    CreateLevel(levelData, ".large-enemy", "large-enemy");
+    CreateLevel(levelData, ".circle", "circle");
+
+
+    if (levelData.length === 0) {
+      alert("The level is empty. Add some blocks before saving.");
+      return;
+    }
+
+    if (!levelId) {
+      alert("Pleave enter a level ID");
+      return;
+    }
+
+    $.ajax({
+      url: "http://localhost:3000/level/" + encodeURIComponent(levelId),
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(levelData),
+      success: function (response) {
+        alert(response);
+        loadLevelList();
+      },
+      error: function (xhr, status, error) {
+        alert("Error saving level: " + xhr.responseText);
+      },
+    });
+  });
+
+  $("#rename-level").click(() => {
+    const options = $("#additive-options");
+    const renameInput = $(`
                 <form name="rename-form" id="name-input">
                     <input type='text' id='new-level-id' placeholder='New Level ID'>
-                    <input type='submit'>
+                    <input type='submit' id='submit-new-name'>
                 </form>
             `);
-        const levelSelected = $("<select id='level-list'> <option value=''>Select a level</option> </select>");
-        
-        if(document.getElementById("new-level-id") === null)
-        {
-            options.append(levelSelected);
-            options.append(renameInput);
-            loadLevelList();
-            document.querySelector("#rename-level").innerHTML = "Cancel";
-            $("form").submit(function(event) {
-                event.preventDefault();
-                const newLevelId = $("#new-level-id").val().trim()        
-                const selectedElement = document.querySelector('#level-list');
-                const level = selectedElement.options[selectedElement.selectedIndex].value;
-                
-                if(!newLevelId)
-                {
-                    alert("Please enter a valid name.")
-                    return;
-                }    
-    
-                $.ajax({
-                    url: 'http://localhost:3000/level/' + encodeURIComponent(level),
-                    method: 'PUT',
-                    contentType: "application/json",
-                    data: JSON.stringify({"name": newLevelId}),
-                    success: function (response) {
-                        alert(response);
-                        loadLevelList();
-                    },
-                    error: function (xhr, status, error) {
-                        alert("Error renaming level: " + xhr.responseText);
-                    }
-                });  
-            })   
-            return;
+    const levelSelected = $(
+      "<select id='level-list'> <option value=''>Select a level</option> </select>"
+    );
+
+    if (document.getElementById("new-level-id") === null) {
+      options.append(levelSelected);
+      options.append(renameInput);
+      loadLevelList();
+      document.querySelector("#rename-level").innerHTML = "Cancel";
+      $("form").submit(function (event) {
+        event.preventDefault();
+        const newLevelId = $("#new-level-id").val().trim();
+        const selectedElement = document.querySelector("#level-list");
+        const level =
+          selectedElement.options[selectedElement.selectedIndex].value;
+
+        if (!newLevelId) {
+          alert("Please enter a valid name.");
+          return;
         }
 
-        document.querySelector("#additive-options").removeChild(document.querySelector("#name-input"));
-        document.querySelector("#additive-options").removeChild(document.querySelector("#level-list"));
-        document.querySelector("#rename-level").innerHTML = "Rename";
-    });
+        $.ajax({
+          url: "http://localhost:3000/level/" + encodeURIComponent(level),
+          method: "PUT",
+          contentType: "application/json",
+          data: JSON.stringify({ name: newLevelId }),
+          success: function (response) {
+            alert(response);
+            removeTempLevelList();
+          },
+          error: function (xhr, status, error) {
+            alert("Error renaming level: " + xhr.responseText);
+          },
+        });
+      });
+      return;
+    }
+    removeTempLevelList();
+  });
 
-    $("#delete-level").click( () => {
-        const options = $("#destructive-options");
-        const selectedElement = document.querySelector('#level-list');
-        const level = selectedElement.options[selectedElement.selectedIndex].value;
-
-        if(!level) {
-            alert("No level was selected to delete.");
-            return;
-        }
-
-        if(confirm(`are you sure you want to delete ${level}`))
-        {
-            $.ajax({
-                url: 'http://localhost:3000/level/' + encodeURIComponent(level),
-                method: 'DELETE',
-                contentType: "application/json",
-                success: function (response) {
-                    alert(response);
-                    clearBlocks();
-                    loadLevelList();
-                },
-                error: function (xhr, status, error) {
-                    alert("error renaming level: " + xhr.responseText);
-                }
-            })
-        }
-    });
-
+  function removeTempLevelList() {
+    document
+      .querySelector("#additive-options")
+      .removeChild(document.querySelector("#name-input"));
+    document
+      .querySelector("#additive-options")
+      .removeChild(document.querySelector("#level-list"));
+    document.querySelector("#rename-level").innerHTML = "Rename";
     loadLevelList();
+  }
+
+  $("#delete-level").click(() => {
+    const options = $("#destructive-options");
+    const selectedElement = document.querySelector("#level-list");
+    const level = selectedElement.options[selectedElement.selectedIndex].value;
+
+    if (!level) {
+      alert("No level was selected to delete.");
+      return;
+    }
+
+    if (confirm(`are you sure you want to delete ${level}`)) {
+      $.ajax({
+        url: "http://localhost:3000/level/" + encodeURIComponent(level),
+        method: "DELETE",
+        contentType: "application/json",
+        success: function (response) {
+          alert(response);
+          clearBlocks();
+          loadLevelList();
+        },
+        error: function (xhr, status, error) {
+          alert("error renaming level: " + xhr.responseText);
+        },
+      });
+    }
+  });
+
+  loadLevelList();
 });
